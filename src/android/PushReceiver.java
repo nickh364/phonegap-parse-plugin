690b074e88b88f99f0a3e4f98e4615d2b231523f
nickh364 <nickh364@users.noreply.github.com>
2014-05-01 08:29:07 -0500

    added title

--- src/android/PushReceiver.java
+++ src/android/PushReceiver.java
@@ -1,5 +1,6 @@
 package org.apache.cordova.core;
 import org.apache.cordova.core.*;
+
 import java.util.Iterator;
 
 import org.json.JSONException;
@@ -11,27 +12,32 @@ import android.content.Intent;
 import android.util.Log;
 
 public class PushReceiver extends BroadcastReceiver {
-	private static final String TAG = "PushReceiver";
-	 
-	  @Override
-	  public void onReceive(Context context, Intent intent) {
-	    try {
-	      String action = intent.getAction();
-	      String channel = intent.getExtras().getString("com.parse.Channel");
-	      JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
-	 
-	      Log.d(TAG, "got action " + action + " on channel " + channel + " with:");
-	      Iterator itr = json.keys();
-	      while (itr.hasNext()) {
-	        String key = (String) itr.next();
-	        if(key.equals("url")){
-	        	Log.d(TAG, "url");
-	        	ParsePlugin.key = json.getString(key);
-	        }
-	        Log.d(TAG, "..." + key + " => " + json.getString(key));
-	      }
-	    } catch (JSONException e) {
-	      Log.d(TAG, "JSONException: " + e.getMessage());
-	    }
-	  }
-	}
+    private static final String TAG = "PushReceiver";
+
+    @
+    Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            String action = intent.getAction();
+            String channel = intent.getExtras().getString("com.parse.Channel");
+            JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
+
+            Log.d(TAG, "got action " + action + " on channel " + channel + " with:");
+            Iterator itr = json.keys();
+            while (itr.hasNext()) {
+                String key = (String) itr.next();
+                if (key.equals("alert")) {
+                    Log.d(TAG, "alert");
+                    ParsePlugin.title = json.getString(key);
+                }
+                if (key.equals("url")) {
+                    Log.d(TAG, "url");
+                    ParsePlugin.key = json.getString(key);
+                }
+                Log.d(TAG, "..." + key + " => " + json.getString(key));
+            }
+        } catch (JSONException e) {
+            Log.d(TAG, "JSONException: " + e.getMessage());
+        }
+    }
+}
